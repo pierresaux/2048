436672ba55fc799719e5e4b285cb1e758f7cf312
pierresaux <pierresaux@ymail.com>
2014-03-27 23:59:40 +0800

    Reworked into Programming Languages?

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -96,7 +96,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value + 1;
+  inner.textContent = tiles[tile.value];
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
